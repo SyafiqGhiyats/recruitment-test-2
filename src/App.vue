d677c3bf18aba31ed9b13806d50cc54054ff7c1b
SyafiqGhiyats <haryoarul@gmail.com>
2021-09-09 14:03:05 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="min-h-screen root">
+    <div class="max-width min-h-screen">
+      <router-view></router-view>
     </div>
-    <router-view />
+    <div v-for="i in 20" :key="i" class="random-glass__wrapper">
+      <div
+        class="glass absolute"
+        :style="{
+          left: `${genNumber(40, 1440)}px`,
+          top: `${genNumber(20, 1107)}px`,
+          width: `${genNumber(70, 200)}px`,
+          height: `${genNumber(70, 200)}px`,
+          transform: `rotate(${genNumber(0, 360)}deg)`,
+          opacity: `${genNumber(10, 70)}%`,
+        }"
+      ></div>
+    </div>
+    <Snackbar v-model="snackbar.show" :danger="snackbar.danger">
+      {{ snackbar.message }}
+    </Snackbar>
   </div>
 </template>
+<script>
+import Snackbar from "./components/Snackbar.vue";
+export default {
+  components: { Snackbar },
+  data() {
+    return {
+      snackbar: {
+        show: false,
+        message: "",
+        danger: false,
+      },
+    };
+  },
+  watch: {
+    snackbar() {
+      setTimeout(() => (this.snackbar.show = false), 4000);
+    },
+  },
+  methods: {
+    genNumber(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+  },
+  mounted() {
+    this.bus.$on("snackbar", (opt) => (this.snackbar = opt));
+  },
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.root {
+  background-image: linear-gradient(to top left, #cc208e 0%, #6713d2 100%);
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.random-glass__wrapper {
+  position: absolute;
+  inset: 0;
+  overflow: hidden;
+  pointer-events: none;
+  z-index: 1;
+  & .glass {
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="center">
+    <div class="title">
+      <h1>Login Page</h1>
+      <div class="glass absolute inset-0"></div>
+    </div>
+    <div class="max-width glass center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolor,
+      vero tenetur commodi delectus temporibus ullam iste iure! Ex obcaecati
+      nulla deleniti, sed assumenda doloremque tempora iusto ea culpa libero?
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
